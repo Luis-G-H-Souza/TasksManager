56e6128a538f0fb2e51f131dd2fa653d082e50fc
Luis <luisouza.gu10@gmail.com>
2024-11-17 17:45:07 -0300

    build: Adding consign

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const app = require('express')()
 const knex = require('knex')
 const knexfile = require('./knexfile')
+const consign = require('consign')
 
 app.db = knex(knexfile.test)
 
 app.use(app.json())
+
+consign({ cwd: 'src', verbose: false })
+  .include('./services')
+  .then('./routes')
+  .then('./config/routes.js')
+  .into(app)
 
 app.get('/', (req, res) => {
   res.status(200).send()
